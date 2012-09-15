9e8d71b266d0a811d7c41f221bf78b0775a1e7c2
Paul Gribble <paul@gribblelab.org>
2012-09-15 14:58:11 -0400

    added repo

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 CompNeuro
 =========
 
+Neuroscience 9520
 Computational Modelling in Neuroscience
+
+Paul Gribble
+paul@gribblelab.org
+www.gribblelab.org
+
---
+++ go.el
@@ -0,0 +1,52 @@
+(require 'org-publish)
+(require 'org-exp-bibtex)
+(setq org-publish-project-alist
+      '(
+	("CompNeuro"
+	 :base-directory "~/Dropbox/Teaching/CompNeuro/fall_2012/notes/org/"
+	 :base-extension "org"
+	 :publishing-directory "~/Dropbox/Teaching/CompNeuro/fall_2012/notes/html"
+	 :Publishing-function org-publish-org-to-html
+	 :recursive t
+	 :section-numbers nil
+	 :html-postamble "<hr />%a | %d<br>This <span xmlns:dct=\"http://purl.org/dc/terms/\" href=\"http://purl.org/dc/dcmitype/Text\" rel=\"dct:type\">work</span> is licensed under a <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-sa/3.0/\">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a><br><a rel=\"license\" href=\"http://creativecommons.org/licenses/by-nc-sa/3.0/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png\" /></a><br />"
+	 :language en
+	 :link-home "index.html"
+	 :link-up "index.html"
+	 :style "<link rel=\"stylesheet\" type=\"text/css\" href=\"mystyle.css\" />"
+	 )
+	("CompNeuro_html"
+	 :base-directory "~/Dropbox/Teaching/CompNeuro/fall_2012/notes/html/"
+	 :base-extension "css\\|html"
+	 :publishing-directory "/ssh:admin@leviathan.ssc.uwo.ca:~/gribblelab.org/compneuro/"
+	 :publishing-function org-publish-attachment
+	 :recursive t
+	 )
+	("CompNeuro_bibhtml"
+	 :base-directory "~/Dropbox/Teaching/CompNeuro/fall_2012/notes/org/"
+	 :base-extension "html\\|css"
+	 :publishing-directory "/ssh:admin@leviathan.ssc.uwo.ca:~/gribblelab.org/compneuro/"
+	 :publishing-function org-publish-attachment
+	 :recursive t
+	 )
+	("CompNeuro_figs"
+	 :base-directory "~/Dropbox/Teaching/CompNeuro/fall_2012/notes/figs/"
+	 :base-extension "png\\|jpg"
+	 :publishing-directory "/ssh:admin@leviathan.ssc.uwo.ca:~/gribblelab.org/compneuro/figs/"
+	 :publishing-function org-publish-attachment
+	 :recursive t
+	 )
+	("CompNeuro_code"
+	 :base-directory "~/Dropbox/Teaching/CompNeuro/fall_2012/notes/code/"
+	 :base-extension "c\\|h\\|txt\\|csv"
+	 :publishing-directory "/ssh:admin@leviathan.ssc.uwo.ca:~/gribblelab.org/compneuro/code/"
+	 :publishing-function org-publish-attachment
+	 :recursive t
+	 )
+	("org" :components ("CompNeuro" "CompNeuro_html" "CompNeuro_bibhtml" "CompNeuro_figs" "CompNeuro_code"))))
+
+(org-publish-project "CompNeuro")
+(org-publish-project "CompNeuro_html")
+(org-publish-project "CompNeuro_bibhtml")
+(org-publish-project "CompNeuro_figs")
+(org-publish-project "CompNeuro_code")
