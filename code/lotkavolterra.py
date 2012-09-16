cf364ed6312f2da2723579e847ee2c1d97843a32
Paul Gribble <paul@gribblelab.org>
2012-09-16 14:55:48 -0400

    misc

---
+++ code/lotkavolterra.py
@@ -0,0 +1,43 @@
+from scipy.integrate import odeint
+
+def LotkaVolterra(state,t):
+  x = state[0]
+  y = state[1]
+  alpha = 0.1
+  beta =  0.1
+  sigma = 0.1
+  gamma = 0.1
+  xd = x*(alpha - beta*y)
+  yd = -y*(gamma - sigma*x)
+  return [xd,yd]
+
+t = arange(0,500,1)
+state0 = [0.5,0.5]
+state = odeint(LotkaVolterra,state0,t)
+figure()
+plot(t,state)
+ylim([0,8])
+xlabel('Time')
+ylabel('Population Size')
+legend(('x (prey)','y (predator)'))
+title('Lotka-Volterra equations')
+
+# animation in state-space
+fig,ax = subplots()
+pb, = ax.plot(state[:,0],state[:,1],'b-',alpha=0.2)
+xlabel('x (prey population size)')
+ylabel('y (predator population size)')
+p, = ax.plot(state[0:10,0],state[0:10,1],'b-')
+pp, = ax.plot(state[10,0],state[10,1],'b.',markersize=10)
+tt = title("%4.2f sec" % 0.00)
+
+# animate
+step=3
+for i in xrange(1,shape(state)[0]-10,step):
+  p.set_xdata(state[10+i:20+i,0])
+  p.set_ydata(state[10+i:20+i,1])
+  pp.set_xdata(state[19+i,0])
+  pp.set_ydata(state[19+i,1])
+  tt.set_text("%d steps" % (i))
+  draw()
+
