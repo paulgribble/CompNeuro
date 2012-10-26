335eb5f9a37f6a9d6088c8cc8c3f1e9f17b4915a
Paul Gribble <paul@gribblelab.org>
2012-10-26 16:48:22 -0400

    5

---
+++ code/twojoint_lagrange.py
@@ -0,0 +1,53 @@
+from sympy import *
+
+m1,m2,r1,r2,i1,i2,l1,l2,a1,a2,t,g = symbols('m1 m2 r1 r2 i1 i2 l1 l2 a1 a2 t g')
+
+# positions
+x1 = r1*sin(a1(t))
+y1 = -r1*cos(a1(t))
+x2 = l1*sin(a1(t)) + r2*sin(a1(t)+a2(t))
+y2 = -l1*cos(a1(t)) - r2*cos(a1(t)+a2(t))
+
+# velocities
+x1d = diff(x1,t)
+y1d = diff(y1,t)
+x2d = diff(x2,t)
+y2d = diff(y2,t)
+a1d = diff(a1(t),t)
+a2d = diff(a2(t),t)
+
+# linear kinetic energy
+Tlin1 = 0.5 * m1 * ((x1d*x1d) + (y1d*y1d))
+Tlin2 = 0.5 * m2 * ((x2d*x2d) + (y2d*y2d))
+
+# rotational kinetic energy
+Trot1 = 0.5 * i1 * a1d * a1d
+Trot2 = 0.5 * i2 * a2d * a2d
+
+# total kinetic energy
+T = Tlin1 + Tlin2 + Trot1 + Trot2
+
+# potential energy
+U1 = m1 * g * ( r1*(1-cos(a1(t))) )
+U2 = m2 * g * ( l1*(1-cos(a1(t))) + r2*(1-cos(a1(t)-a2(t))) )
+U = U1 + U2
+
+# lagrangian L
+L = T - U
+
+# compute generalized forces (toruqes) Qj
+Q1 = diff(diff(L,diff(a1(t))),t) - diff(L,a1(t))
+Q2 = diff(diff(L,diff(a2(t))),t) - diff(L,a2(t))
+
+# simplify!
+Q1 = simplify(nsimplify(Q1))
+Q2 = simplify(nsimplify(Q2))
+Q1 = collect(Q1, sin(a1(t)))
+Q2 = collect(Q2, sin(a1(t)))
+Q1 = collect(Q1, Derivative(Derivative(a1(t),t),t))
+Q1 = collect(Q1, Derivative(Derivative(a2(t),t),t))
+Q2 = collect(Q2, Derivative(Derivative(a1(t),t),t))
+Q2 = collect(Q2, Derivative(Derivative(a2(t),t),t))
+
+pprint(Q1)
+pprint(Q2)
