5ab96b9659ac2b7f69f0649c08b7cab2f7fda3da
Paul Gribble <paul@gribblelab.org>
2012-11-09 23:41:06 -0500

    code

--- code/xor_cg.py
+++ code/xor_cg.py
@@ -23,13 +23,27 @@ def dtansig(x):
 	""" derivative of sigmoid function """
 	return 1.0 - (multiply(x,x)) # element-wise multiplication
 
-def net_forward(x, params):
-	""" propagate inputs through the network and return outputs """
+def pack_weights(w_hid, w_out, params):
+	""" pack weight matrices into a single vector """
+	n_in, n_hid, n_out = params[0], params[1], params[2]
+	g_j = hstack((	reshape(w_hid,(1,n_in*n_hid)), 
+		     		reshape(w_out,(1,n_hid*n_out))))[0]
+	g_j = array(g_j[0,:])[0]
+	return g_j
+
+def unpack_weights(x, params):
+	""" unpack weights from single vector into weight matrices """
 	n_in, n_hid, n_out = params[0], params[1], params[2]
 	pat_in, pat_out = params[3], params[4]
 	n_pat = shape(pat_in)[0]
 	wgt_hid = reshape(x[0:n_in*n_hid], (n_in,n_hid))
 	wgt_out = reshape(x[n_in*n_hid:], (n_hid,n_out))
+	return wgt_hid, wgt_out
+
+def net_forward(x, params):
+	""" propagate inputs through the network and return outputs """
+	wgt_hid, wgt_out = unpack_weights(x, params)
+	pat_in = params[3]
 	return tansig(tansig(pat_in * wgt_hid) * wgt_out)
 
 def f(x,params):
@@ -41,9 +55,7 @@ def fd(x,params):
 	""" returns the gradients (dW/dE) for the weight vector """
 	n_in, n_hid, n_out = params[0], params[1], params[2]
 	pat_in, pat_out = params[3], params[4]
-	n_pat = shape(pat_in)[0]
-	wgt_hid = reshape(x[0:n_in*n_hid], (n_in,n_hid))
-	wgt_out = reshape(x[n_in*n_hid:], (n_hid,n_out))
+	wgt_hid, wgt_out = unpack_weights(x, params)
 	act_hid = tansig( pat_in * wgt_hid )
 	act_out = tansig( act_hid * wgt_out )
 	err_out = act_out - pat_out
@@ -52,10 +64,7 @@ def fd(x,params):
 	deltas_hid = multiply(dtansig(act_hid), err_hid)
 	grad_out = transpose(act_hid)*deltas_out
 	grad_hid = transpose(pat_in)*deltas_hid
-	g_j = hstack((reshape(grad_hid,(1,n_in*n_hid)), 
-		      reshape(grad_out,(1,n_hid*n_out))))[0]
-	g_j = array(g_j[0,:])[0]
-	return g_j
+	return pack_weights(grad_hid, grad_out, params)
 
 ##########################
 #     the good stuff     #
